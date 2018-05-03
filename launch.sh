789c860ab15e29cc302f0d61350e88440e9acea4
NOVAR1 <30316002+NOVAR1@users.noreply.github.com>
2018-05-03 19:46:20 +0300

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,64 +1,26 @@
-#!/usr/bin/env bash
+THIS_DIR=$(cd $(dirname $0); pwd)
+cd $THIS_DIR
 
-## VARIABLES
-THIS_DIR=$(cd "$(dirname "$0")"; pwd)
-TDCLI='https://valtman.name/files/telegram-cli-1222'
-
-## Print Colorful
-# Print text in red
-prtred() {
-  printf '\e[1;31m%s\n\e[0;39;49m' "$@"
-}
-# Print text in green
-prtgrn() {
-  printf '\e[1;32m%s\n\e[0;39;49m' "$@"
-}
-# Print text in brown
-prtbrown() {
-  printf '\e[1;33m%s\n\e[0;39;49m' "$@"
+install() {
+      wget "https://valtman.name/files/telegram-cli-1222"
+     mv telegram-cli-1222 tg
+     sudo chmod +x tg
+echo -e "———————————————————————————————————————————————————————————\n"
+      echo -e "      \n"
+echo -e "———————————————————————————————————————————————————————————\n"
 }
 
-# install Bot
+if [ "$1" = "install" ]; then
+install
+else
+if [ ! -f ./tg ]; then
+echo "TG not found"
+echo "Run $0 install"
+exit 1
+fi
+./tg -s bot.lua
+fi
 
-      prtgrn "
- >> Fetching $TDCLI
-"
-      wget "$TDCLI" -O TG
-      chmod +x TG
-      prtgrn "
- ➡[ @llx8xll ] 
-"
-      sudo apt-get -y update && sudo apt-get -y upgrade 
-      prtgrn "
- ➡[ @llx8xll ] 
-"
-      sudo apt-get --force-yes install git wget screen tmux libconfig9 libevent-dev libjansson4 libstdc++6 lua-socket lua5.2 liblua5.2 make unzip redis-server lua-sec libnotify-dev software-properties-common g++
-      sudo apt-get -y update && sudo apt-get -y upgrade
-      sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test && sudo apt-get update && sudo apt-get install -y gcc-4.9 g++-4.9 && sudo update-alternatives —install /usr/bin/gcc gcc /usr/bin/gcc-4.9 60 —slave /usr/bin/g++ g++ /usr/bin/g++-4.9
-      sudo apt-get install lua-lgi -y
-      wget http://luarocks.org/releases/luarocks-2.2.2.tar.gz
-      tar zxpf luarocks-2.2.2.tar.gz
-      cd luarocks-2.2.2
-      ./configure; sudo make bootstrap
-      sudo luarocks install luasocket
-      sudo luarocks install luasec
-      sudo luarocks install serpent
-      sudo luarocks install redis-lua
-      sudo luarocks install lua-term
-      sudo luarocks install serpent
-      sudo luarocks install dkjson
-      sudo luarocks install lanes
-      sudo luarocks install Lua-cURL
-	  sudo luarocks install lua-cjson
-	  sudo luarocks install fakeredis
-	  sudo luarocks install ltn12
-      cd ..
-	  rm -fr luarocks-2.2.2.tar.gz
-	  rm -fr luarocks-2.2.2
-	  lua ./bot.lua
-
-	  
-	  
 	  
 	  
 	  
