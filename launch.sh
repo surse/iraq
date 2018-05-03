540903d94c0c459d1f550ae3c25804468771a73a
NOVAR1 <30316002+NOVAR1@users.noreply.github.com>
2018-05-04 00:20:51 +0300

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,29 +1,26 @@
 THIS_DIR=$(cd $(dirname $0); pwd)
 cd $THIS_DIR
-
 install() {
-      wget "https://valtman.name/files/telegram-cli-1222"
-     mv telegram-cli-1222 tg
-     sudo chmod +x tg
-echo -e "———————————————————————————————————————————————————————————\n"
-      echo -e "      \n"
-echo -e "———————————————————————————————————————————————————————————\n"
+wget "https://valtman.name/files/telegram-cli-1222"
+mv telegram-cli-1222 tg
+sudo chmod +x tg
+echo -e "
+ __   ___________   ___     _____     __  __
+/  |  |___   ___|  / _ \   | ___ }   |  \/  |
+\_ \      | |     | | | |  | |_) }   | |\/| |
+ _) |     | |     | |_| |  |  _< \   | |  | |
+|__/      |_|      \___/   |_|  \_\  |_|  |_|
+ BY @TAHAJ20 DEV : TAHAJ20            "
+echo -e "" 
+echo -e "" 
 }
-
 if [ "$1" = "install" ]; then
 install
 else
 if [ ! -f ./tg ]; then
-echo "TG not found"
-echo "Run $0 install"
+echo "" 
+echo ""
 exit 1
 fi
 ./tg -s bot.lua
 fi
-
-	  
-	  
-	  
-    
-
-
